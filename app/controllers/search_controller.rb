0d431bffce1813703a056ed8a9d45d2639fcf293
“Krista <stadler.krista@gmail..com>
2020-05-19 10:30:19 -0600

    Successfully connected to API and returned results to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
   def index
-    house = params[:house]
+    @house = params[:house]
 
     conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
       faraday.headers['Content-Type'] = 'application/json'
     end
 
-    response = conn.get("characters?key=#{ENV['POTTER_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
-    require "pry"; binding.pry
+    response = conn.get("characters?key=#{ENV['POTTER_API_KEY']}&house=#{@house}&orderOfThePhoenix=true")
+    @members = JSON.parse(response.body, symbolize_names: true)
+
   end
 end
